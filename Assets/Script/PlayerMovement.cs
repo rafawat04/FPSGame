7b4ef0d07382c2b1da4904818fe5e87006e7f8f0
Rafael Watanabe <rafawat0428@gmail.com>
2023-02-18 18:23:13 +0900

    Camera work/script

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -2,20 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 vector = new Vector3();
-
-        vector.x = Input.GetAxis("Horizontal");
-        vector.z = Input.GetAxis("Vertical");
+        
     }
 }
